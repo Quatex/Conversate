ba615bd469e57f7598c8df89f2d14698582cd00f
William Zhang <W.YkZhang@hotmail.com>
2019-03-21 21:22:46 -0400

    update

--- Python1.py
+++ Python1.py
@@ -13,11 +13,19 @@ from sklearn import svm, cross_validation, neighbors
 from sklearn.ensemble import VotingClassifier, RandomForestClassifier
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
+from keras.models import Sequential, load_model
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+from sklearn.preprocessing import MinMaxScaler
+
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning) 
 
 
 style.use('ggplot')
+
+
 
 # allstock = pd.read_csv('all_stocks_5yr.csv')
 # print(allstock.head())
@@ -63,6 +71,7 @@ def visualize_data():
     heatmap.set_clim(-1,1)
     plt.tight_layout()
     plt.show()
+#visualize_data()
 
 def process_data_for_labels(ticker):
     hm_days = 7
@@ -76,6 +85,12 @@ def process_data_for_labels(ticker):
     df.fillna(0,inplace=True)
     return tickers,df
 
+
+# def process(ticker):
+#     number_days = 60
+#     df = pd.read_csv('modifiedsp500.csv', index_col = 0)
+#     tickers = df.columns.values.to.list()
+#     df.fillna(0, inplace=True)
 
 def buy_sell_hold(*args):
     cols = [c for c in args]
@@ -128,12 +143,289 @@ def do_ml(ticker):
     predictions = clf.predict(X_test)
     print('Predicted spread:', Counter(predictions))
 
-    # mat = confusion_matrix(y_test, predictions)
-    # sns.heatmap(mat, square=True, annot=True, cbar=False)
-    # plt.xlabel('predicted value')
-    # plt.ylabel('true value');
+    mat = confusion_matrix(y_test, predictions)
+    sns.heatmap(mat, square=True, annot=True, cbar=False)
+    plt.xlabel('predicted value')
+    plt.ylabel('true value')
 
-    # plt.show()
+    plt.show()
     return confidence
 
-do_ml('BAC')
+# do_ml('AAPL')
+
+
+dataset_train = pd.read_csv('sp500_joined_close.csv')
+training_set = dataset_train.iloc[:, 1:]. values
+
+print(training_set)
+np.nan_to_num(training_set, copy=False)
+
+# train_test_split_amount = 0.90
+# train_size = int(len(training_set) * train_test_split_amount)
+# x_train, x_test = training_set[0:train_size], training_set[train_size:len(training_set)]
+# print('Observations: %d' % (len(training_set)))
+# print('Training Observations: %d' % (len(x_train)))
+# print('Testing Observations: %d' % (len(x_test)))
+
+sc = MinMaxScaler(feature_range = (0,1))
+training_set_scaled = sc.fit_transform(training_set)
+print(training_set.shape)
+
+time_steps = 60
+X_train = []
+y_train = []
+for x in range(training_set_scaled.shape[1]):
+    X_train_temp = []
+    y_train_temp = []
+    for i in range(time_steps, training_set_scaled.shape[0]):
+        X_train_temp.append(training_set_scaled[i - time_steps:i, x])
+        y_train_temp.append(training_set_scaled[i,x])
+    X_train.append(X_train_temp)
+    y_train.append(y_train_temp)
+
+with open("sp500tickers.pickle", "rb") as f:
+    tickers = pickle.load(f)
+print(type(tickers))
+
+ticker = "ABBV"
+index = tickers.index(ticker)
+print(index)
+
+for i in range(time_steps, training_set_scaled.shape[0]):
+        y_train.append(training_set_scaled[i,index])
+
+y_train = np.array(y_train)
+X_train = np.array(X_train)
+
+X_train = np.reshape(X_train, (X_train.shape[1], X_train.shape[2], X_train.shape[0]))
+y_train = np.reshape(y_train, (y_train.shape[1], y_train.shape[0]))
+print(X_train.shape)
+print(y_train.shape)
+print(x_test.shape)
+
+regressor = Sequential()
+
+regressor.add(LSTM(units = 150, return_sequences = True, input_shape = (X_train.shape[1],X_train.shape[2])))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units = 150, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units = 150, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units = 150))
+regressor.add(Dropout(0.2))
+
+regressor.add(Dense(units = y_train.shape[1]))
+
+regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+
+
+
+X_train.shape
+y_train.shape
+
+regressor.fit(X_train, y_train, epochs= 100, batch_size = 100)
+
+regressor.save('my_model.h5')
+regressor.save_weights('my_model_weights.h5')
+
+
+regressor = load_model('train_1980.h5')
+
+dataset_test = pd.read_csv('TEST_sp500_joined_close_TEST.csv')
+real_stock_price = dataset_test.iloc[:, 1:].values
+
+
+
+
+
+
+total_dataset = pd.concat([dataset_train, dataset_test])
+np.nan_to_num(total_dataset, copy=False)
+inputs = total_dataset[len(total_dataset) - len(dataset_test) - 60:].values
+print(inputs.shape)
+print(inputs)
+training_set_scaled = sc.fit_transform(x_train)
+
+
+time_steps = 60
+X_test = []
+y_test = []
+for x in range(x_test.shape[1]):
+    X_test_temp = []
+    y_test_temp = []
+    for i in range(time_steps, x_test.shape[0]):
+        X_test_temp.append(x_test[i - time_steps:i, x])
+        y_test_temp.append(x_test[i,x])
+    X_test.append(X_test_temp)
+    y_test.append(y_test_temp)
+
+y_test = np.array(y_test)
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[1], X_test.shape[2], X_test.shape[0]))
+y_test = np.reshape(y_test, (y_test.shape[1], y_test.shape[0]))
+print(X_test.shape)
+print(y_test.shape)
+
+# with open("sp500tickers.pickle", "rb") as f:
+#     tickers = pickle.load(f)
+# print(type(tickers))
+
+# ticker = "ABBV"
+# index = tickers.index(ticker)
+# print(index)
+
+# for i in range(time_steps, training_set_scaled.shape[0]):
+#         y_train.append(training_set_scaled[i,index])
+
+y_test = np.array(y_test)
+X_test = np.array(X_test)
+
+X_train = np.reshape(X_train, (X_train.shape[1], X_train.shape[2], X_train.shape[0]))
+y_train = np.reshape(y_train, (y_train.shape[1], y_train.shape[0]))
+print(X_train.shape)
+print(y_train.shape)
+
+
+
+##########################################################
+
+TOTAL_DATASET = pd.read_csv('sp500_joined_close.csv')
+real_stock_price_unscaled = TOTAL_DATASET.iloc[5000:, 1:].values
+np.nan_to_num(real_stock_price_unscaled, copy=False)
+
+sc = MinMaxScaler(feature_range = (0,1))
+sc.fit(real_stock_price_unscaled)
+
+real_stock_price = sc.transform(real_stock_price_unscaled)
+
+print(real_stock_price[real_stock_price.shape[0] - 49:, 49])
+
+train_test_split_amount = 0.99
+train_size = int(len(real_stock_price) * train_test_split_amount)
+trainset_x, testset_x = real_stock_price[0:train_size], real_stock_price[train_size- 60:len(real_stock_price)]
+print('Observations: %d' % (len(real_stock_price)))
+print('Training Observations: %d' % (len(trainset_x)))
+print('Testing Observations: %d' % (len(testset_x)))
+
+# trainset_x = sc.transform(trainset_x)
+# testset_x = sc.transform(testset_x)
+print(trainset_x)
+
+def preprocess_FR(inputs, time_steps = 60):
+    X_test = []
+    y = 0
+    for x in range(inputs.shape[1]):
+        X_test_temp = []
+        for i in range(time_steps, inputs.shape[0]):
+            X_test_temp.append(inputs[i - time_steps:i, x])
+        X_test_temp = np.array(X_test_temp)
+        if X_test == []:
+            X_test = X_test_temp
+        elif y == 0:
+            X_test = np.stack((X_test, X_test_temp), axis = 2)
+            y += 1
+        else:
+            X_test_temp = np.reshape(X_test_temp, (X_test_temp.shape[0], X_test_temp.shape[1],1))
+            X_test = np.append(X_test, X_test_temp, axis = 2)
+    y_test = inputs[inputs.shape[0] - time_steps:, :]
+    print(X_test.shape)
+    return X_test, y_test
+
+X_train1, y_train1 = preprocess_FR(trainset_x)
+X_test1, y_test1 = preprocess_FR(testset_x)
+
+y_test1 = testset_x[-49:, :]
+print(testset_x.shape)
+print(X_train1)
+print(y_train1)
+print(type(y_test1))
+X_test1.shape
+y_test1.shape
+y_test1 = np.array(y_test1)
+print(y_test1[:,49])
+
+regressor.fit(X_train1, y_train1, epochs= 100, batch_size = 100)
+
+regressor.save('train_1980.h5')
+regressor.save_weights('train_1980_weights.h5')
+
+############### DISPLAY GRAPH ##############
+print(X_test1.shape)
+OK_TEST = real_stock_price_unscaled[(real_stock_price_unscaled.shape[0] - X_test1.shape[0] - 60):, 0:]
+
+OK_TEST.shape
+OK_TEST = sc.transform(OK_TEST)
+OK_TESTX, OK_TESTY = preprocess_FR(OK_TEST)
+
+
+print(y_test1[:49])
+y_testTEST = sc.inverse_transform(y_test1)
+print(y_test1[:,49])
+print(y_testTEST[:,49])
+
+predicted_stock_price = regressor.predict(X_test1)
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+y_test1 = sc.inverse_transform(y_test1)
+print(X_test1[:,0,49])
+x_test_reverse = np.reshape(X_test1[:0,49], (-1,1))
+x_test_reverse = sc.inverse_transform(X_test1[:,0,49])
+print(y_test1[:,49])
+
+print(predicted_stock_price[:,49])
+
+ticker_name = 'AAPL'
+with open("sp500tickers.pickle", "rb") as f:
+    tickers = pickle.load(f)
+index = tickers.index(ticker_name)
+
+plt.plot(real_stock_price_unscaled[(real_stock_price_unscaled.shape[0] - X_test1.shape[0]):,index], color = 'red', label = 'Real {} Stock Price'.format(ticker_name))
+plt.plot(predicted_stock_price[:,index], color='blue', label='Predicted {} Stock Price'.format(ticker_name))
+plt.title('{} Stock Price Prediction'.format(ticker_name))
+plt.xlabel('Time in days')
+plt.ylabel('{} Stock Price'.format(ticker_name))
+plt.legend()
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+print(index)
+print(y_test1[:,49].shape)
+print(OK_TESTX[0,0,1])
+OK_TEST.shape
+OK_TESTX.shape
+predicted_stock_price.shape
+OKTESTXX = sc.inverse_transform(OK_TESTX)
+
+WOWOWOWOW = MinMaxScaler(feature_range = (0,1))
+WOWOWOWOW.fit(testset_x)
+TESTING = real_stock_price_unscaled
+TESTING1= WOWOWOWOW.transform(TESTING)
+TESTING1 = WOWOWOWOW.inverse_transform(TESTING1)
+
+for row in range(TESTING.shape[0]):
+    for column in range(TESTING.shape[1]):
+        if TESTING[row, column] != TESTING1 [row, column]:
+            print('TESTING1: ', TESTING1[row, column])
+            print('TESTING1: ', TESTING1[row, column])
+            print("ROW, COLUMN", row, column)
+            break
+        else:
+            continue
+    break
+
+print(TESTING== TESTING1)
+print(TESTING1)
+
+print(TESTING1[0,1])
+print(TESTING[0,1])
--- data.py
+++ data.py
@@ -1,6 +1,8 @@
 import os
 import bs4 as bs
 import matplotlib.pyplot as plt
+from matplotlib import style
+import matplotlib.dates as mdates
 import pandas as pd
 import numpy as np
 import pickle
@@ -10,7 +12,10 @@ import quandl
 import pandas_datareader
 import requests
 import pandas_datareader.data as web
+from mpl_finance import candlestick_ohlc
+import csv
 
+style.use('ggplot')
 quandl.ApiConfig.api_key = 'TrrV5dCZUX4fwAY1UyqU'  # Optional
 #quandl.ApiConfig.api_version = '2015-04-09'  # Optional
 token = "3Z31rgQO0c9w9SyoQBoBs7DVxTVTOObF8Wj1lYEwxpsjX8gl7vaQ7co1UyM0"
@@ -25,6 +30,7 @@ token = "3Z31rgQO0c9w9SyoQBoBs7DVxTVTOObF8Wj1lYEwxpsjX8gl7vaQ7co1UyM0"
 #print(pandas_datareader.get_data_yahoo("GOOG").Close.tail())
 
 def save_sp500_tickers():
+    #for sp500 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
     resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
     soup = bs.BeautifulSoup(resp.text, "lxml")
     table = soup.find('table', {'class':'wikitable sortable'})
@@ -34,32 +40,139 @@ def save_sp500_tickers():
         tickers.append(ticker)
     with open("sp500tickers.pickle", "wb") as f:
         pickle.dump(tickers, f)
-    print(tickers)
     return tickers
 
 #save_sp500_tickers()
 
+#first date = 1980, 1,1
 def get_data_from_yahoo(reload_sp500=False):
     if reload_sp500:
         tickers = save_sp500_tickers()
     else:
         with open("sp500tickers.pickle", "rb") as f:
             tickers = pickle.load(f)
-    if not os.path.exists('stock_dfs'):
-        os.makedirs('stock_dfs')
+    if not os.path.exists('stock_test'):
+        os.makedirs('stock_test')
 
-    start = dt.datetime(1980, 1, 1)
+    start = dt.datetime(2019, 2, 23)
     end = dt.datetime.now()
     for ticker in tickers:
         # just in case your connection breaks, we'd like to save our progress!
-        if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
+        if not os.path.exists('stock_test/{}.csv'.format(ticker)):
             df = web.DataReader(ticker, 'yahoo', start, end)
             # df.reset_index(inplace=True)
             # df.set_index("Date", inplace=True)
             # df = df.drop("Symbol", axis=1)
-            df.to_csv('stock_dfs/{}.csv'.format(ticker))
+            df.to_csv('stock_test/{}.csv'.format(ticker))
         else:
             print('Already have {}'.format(ticker))
 
+df = web.DataReader("^HSI", 'yahoo', dt.datetime(2019,2,23), dt.datetime.now())
+print(df.head())
+print(df.tail())
+# get_data_from_yahoo()
 
-get_data_from_yahoo()
+# with open("sp500tickers.pickle", "rb") as f:
+#     current_tickers = pickle.load(f)
+
+# print(current_tickers)
+
+def update_stocks(reload_sp500=False):
+    if reload_sp500:
+        get_data_from_yahoo(reload_sp500)
+
+    if not os.path.exists('test'):
+        print("Fix stock directory")
+    else: 
+        os.chdir(os.getcwd() + '\\test\\')
+        for dirc in os.walk(os.getcwd(), topdown=True):
+            for comps in dirc[2]:
+                print("comps is:", comps)
+                """
+                Open up the csv and read the last line.
+                [-1] accesses the last row of the csv file.
+                [:10] gets the first 10 characters at index 0-9 of the last line.
+                Should be equivalent to the date YYYY-MM-DD = 10 characters.
+                """
+                last_update = open(comps).readlines()[-1][:10]
+
+                # while last_update.isspace():
+                    
+                # if (last_update.isspace()):
+                #     print("line is empty")
+                #     last_update = open(comps).readlines()[-2][:10]
+                # else:
+                #     print("last line is not empty")
+                #     print("last update is:", last_update)
+
+                #Split the string to be put into the datetime class.
+                s_year, s_month, s_day = last_update.split('-')
+
+                #Get today's date.
+                e_year, e_month, e_day = str(dt.datetime.now().date()).split('-')
+                
+                #Check to see if the csv even needs to be updated. If it is move to next file.
+                if(s_year == e_year and s_month == e_month and s_day == e_day):
+                    print(comps,'is up to date.')
+                    continue
+                else:
+
+                    #Create an end and start to put into the pandas dataframe.
+                    end = dt.datetime(int(e_year), int(e_month), int(e_day))
+                    start = dt.datetime(int(s_year), int(s_month), int(s_day))
+
+                    #Get the ticker from the name of the file.
+                    tckr = comps.split('.')[0]
+
+                    #Get the daily stock information between last update and today.
+                    df = web.DataReader(tckr, 'yahoo', start, end)
+
+                    """
+                    Convert the pandas dataframe into a string to be written by csv.
+                    [1:] gets rid of first date because that date is already in csv from previous access.
+                    The [:-1] gets rid of the last string because it is always empty.
+                    header=None removes the column information so we can just take the information we want.
+                    """
+                    to_csv = str(df.to_csv(header=None)).split('\n')[1:-1]
+
+                    #Write the new string to csv.
+                    with open(comps, 'a', newline='') as f:
+                        writer = csv.writer(f, delimiter='\n')
+                        writer.writerow(to_csv)
+
+                        #Confirm that we successfully updated the csv file.
+                        print(comps,'updated')
+
+# update_stocks()
+
+# df = pd.read_csv("test/AAPL.csv")
+# print(df.head())
+# print(df.tail())
+# df.to_csv("test.csv")
+
+
+def group_everything():
+    with open("sp500tickers.pickle", "rb") as f:
+        tickers = pickle.load(f)
+
+    main_df = pd.DataFrame()
+
+    for count, ticker in enumerate(tickers):
+        df = pd.read_csv('stock_test/{}.csv'.format(ticker))
+        df.set_index('Date', inplace= True)
+
+        df.rename(columns = {'Adj Close': ticker}, inplace= True)
+        df.drop(['Open','High','Low','Close','Volume'], 1, inplace= True)
+
+        if main_df.empty:
+            main_df = df
+        else:
+            main_df = main_df.join(df, how= 'outer')
+        
+        if count % 10 == 0:
+            print(count)
+            
+    print(main_df.head())
+    main_df.to_csv('TEST_sp500_joined_close_TEST.csv')
+
+# group_everything()
